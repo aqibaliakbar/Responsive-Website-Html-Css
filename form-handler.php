4778127bfa7c76e21f3407052e43f5619cbb357c
aqibaliakbar <113066947+aqibaliakbar@users.noreply.github.com>
2023-12-09 21:33:33 +0500

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,22 +1,26 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$email_from = 'aqibaliakbar@gmail.com';
-$email_subject ='New Form Submission';
-$email_body = "User Name: = $name./n". 
-"User Email:  $visitor_email./n". 
-"Subject:  $subject./n". 
-"User Message: $message./n";
+    $email_from = 'aqibaliakbar@gmail.com';
+    $email_subject = 'New Form Submission';
+    $email_body = "User Name: $name\n" . 
+                  "User Email: $visitor_email\n" . 
+                  "Subject: $subject\n" . 
+                  "User Message: $message\n";
 
-$to = 'aqibaliakbar@gmail.com';
-$headers = "From: $email_from /r/n";
-$headers = "Reply_To: $visitor_email /r/n";
+    $to = 'aqibaliakbar@gmail.com';
+    $headers = "From: $email_from\n";
+    $headers .= "Reply-To: $visitor_email\n";
 
-mail ($to, $email_subject, $email_body, $headers);
+    mail($to, $email_subject, $email_body, $headers);
 
-header ("Locattion: contact.html");
-
+    header("Location: thank_you.html"); // Redirect to a thank you page
+} else {
+    // Handle invalid requests
+    header("Location: error.html");
+}
 ?>
